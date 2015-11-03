e611fd8c2de414585f9b316565d062f90af63364
Pan, Senshan <pansenshan@aliyun.com>
2015-11-03 15:06:31 +0800

    dired kbds and the feature of opening in apps

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,12 +1,38 @@
 (require-package 'dired+)
 (require-package 'dired-sort)
-
-(setq-default diredp-hide-details-initially-flag nil
+(setq-default diredp-hide-details-initially-flag t
               dired-dwim-target t)
 
 ;; Prefer g-prefixed coreutils version of standard utilities when available
 (let ((gls (executable-find "gls")))
   (when gls (setq insert-directory-program gls)))
+
+(defun xah-open-in-external-app (&optional file)
+  "Open the current file or dired marked files in external app.
+
+The app is chosen from your OS's preference."
+  (interactive)
+  (let ( doIt
+         (myFileList
+          (cond
+           ((string-equal major-mode "dired-mode") (dired-get-marked-files))
+           ((not file) (list (buffer-file-name)))
+           (file (list file)))))
+
+    (setq doIt (if (<= (length myFileList) 5)
+                   t
+                 (y-or-n-p "Open more than 5 files? ") ) )
+
+    (when doIt
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc (lambda (fPath) (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" fPath t t)) ) myFileList))
+       ((string-equal system-type "darwin")
+        (mapc (lambda (fPath) (shell-command (format "open \"%s\"" fPath)) )  myFileList) )
+       ((string-equal system-type "gnu/linux")
+        (mapc (lambda (fPath) (let ((process-connection-type nil)) (start-process "" nil "xdg-open" fPath)) ) myFileList) ) ) ) ) )
+
+(global-set-key (kbd "C-S-o") 'xah-open-in-external-app)
 
 (after-load 'dired
   (require 'dired+)
@@ -15,6 +41,12 @@
     (global-dired-hide-details-mode -1))
   (setq dired-recursive-deletes 'top)
   (define-key dired-mode-map [mouse-2] 'dired-find-file)
+  ;; no corresponding functions
+  ;; (define-key dired-mode-map "|" 'dired-sort-menu-toggle-reverse)
+  ;; (define-key dired-mode-map (kbd "A") 'gnus-dired-attach)
+  (define-key dired-mode-map (kbd "<M-up>") (kbd "^"))
+  (define-key dired-mode-map (kbd "F") 'find-name-dired)
+  (define-key dired-mode-map (kbd "D") '(lambda () (interactive) (setq delete-by-moving-to-trash (not delete-by-moving-to-trash))))
   (add-hook 'dired-mode-hook
             (lambda () (guide-key/add-local-guide-key-sequence "%"))))
 
