9dbd1611bcff037159551f293229a6aff3469662
Pan, Senshan <pansenshan@aliyun.com>
2015-11-04 16:15:02 +0800

    use ergonomic kbds

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -59,7 +59,8 @@
 (global-set-key (kbd "C-\'") 'ido-switch-buffer)
 (global-set-key (kbd "<C-next>") 'next-buffer)
 (global-set-key (kbd "<C-prior>") 'previous-buffer)
-;; (global-set-key (kbd "<C-w>") 'ido-kill-buffer)
-;; (global-set-key (kbd "<C-s>") 'save-buffer)
+(global-set-key (kbd "C-w") 'kill-this-buffer)
+(global-set-key (kbd "C-s") 'save-buffer)
+(global-set-key (kbd "M-s") 'isearch-forward)
 
 (provide 'init-ibuffer)
